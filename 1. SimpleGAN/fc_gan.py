904c30675e708a98f96561f73fcc41a5a08b3ca1
macallen117@gmail.com <qq12370ssqq>
2022-03-14 15:03:10 +0100

    ..

--- 1. SimpleGAN/fc_gan.py
+++ 1. SimpleGAN/fc_gan.py
@@ -12,9 +12,11 @@ class Discriminator(nn.Module):
     def __init__(self, in_features):
         super().__init__()
         self.disc = nn.Sequential(
-            nn.Linear(in_features, 128),
-            nn.LeakyReLU(0.01),
-            nn.Linear(128, 1),
+            nn.Linear(in_features, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
             nn.Sigmoid(),
         )
 
@@ -27,8 +29,15 @@ class Generator(nn.Module):
         super().__init__()
         self.gen = nn.Sequential(
             nn.Linear(z_dim, 256),
-            nn.LeakyReLU(0.01),
-            nn.Linear(256, img_dim),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(256, momentum= 0.8),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(512, momentum=0.8),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2),
+            nn.BatchNorm1d(1024, momentum=0.8),
+            nn.Linear(1024, img_dim),
             nn.Tanh(),  # normalize inputs to [-1, 1] so make outputs [-1, 1]
         )
 
@@ -38,23 +47,26 @@ class Generator(nn.Module):
 
 # Hyperparameters etc.
 device = "cuda" if torch.cuda.is_available() else "cpu"
-lr = 3e-4
+lr = 3e-4 # learning rate works well for adam
 z_dim = 64
 image_dim = 28 * 28 * 1  # 784
 batch_size = 32
-num_epochs = 50
+num_epochs = 500
 
+#initialize disc and gen
 disc = Discriminator(image_dim).to(device)
 gen = Generator(z_dim, image_dim).to(device)
-fixed_noise = torch.randn((batch_size, z_dim)).to(device)
+fixed_noise = torch.randn((batch_size, z_dim)).to(device) # generate noise (batch_size * z_dim)
+# normalize the initial MNIST dataset
 transforms = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),]
 )
-
 dataset = datasets.MNIST(root="dataset/", transform=transforms, download=True)
 loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+# set optimizer to adam
 opt_disc = optim.Adam(disc.parameters(), lr=lr)
 opt_gen = optim.Adam(gen.parameters(), lr=lr)
+# use BCE loss
 criterion = nn.BCELoss()
 writer_fake = SummaryWriter(f"logs/fake")
 writer_real = SummaryWriter(f"logs/real")
@@ -62,26 +74,30 @@ step = 0
 
 for epoch in range(num_epochs):
     for batch_idx, (real, _) in enumerate(loader):
-        real = real.view(-1, 784).to(device)
+        real = real.view(-1, 784).to(device)   # batch_size * 784
         batch_size = real.shape[0]
 
         ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+
+        # loss of batch_size of real data
+        disc_real = disc(real).view(-1) #flatten
+        lossD_real = criterion(disc_real, torch.ones_like(disc_real))  # 1 because its real
+
+        # loss of batch_size of fake data
         noise = torch.randn(batch_size, z_dim).to(device)
-        fake = gen(noise)
-        disc_real = disc(real).view(-1)
-        lossD_real = criterion(disc_real, torch.ones_like(disc_real))
-        disc_fake = disc(fake).view(-1)
-        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+        fake = gen(noise) # batch_size * 784
+        disc_fake = disc(fake).view(-1) # flatten
+        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))  # 0 because its fake
+
         lossD = (lossD_real + lossD_fake) / 2
         disc.zero_grad()
         lossD.backward(retain_graph=True)
         opt_disc.step()
 
         ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
-        # where the second option of maximizing doesn't suffer from
-        # saturating gradients
+        # where the second option of maximizing doesn't suffer from saturating gradients
         output = disc(fake).view(-1)
-        lossG = criterion(output, torch.ones_like(output))
+        lossG = criterion(output, torch.ones_like(output))  # 1 try to let generator think its true
         gen.zero_grad()
         lossG.backward()
         opt_gen.step()
